0bd7ec6fa020cae333f3cd7dc50f10bb0a4ad3ca
BrijeshDevOp <brijesh9663dev@gmail.com>
2025-08-18 23:15:45 +0530

    done with UI

--- app/src/main/java/com/codewithkael/webrtcdatachannelty/ui/SplashActivity.kt
+++ app/src/main/java/com/codewithkael/webrtcdatachannelty/ui/SplashActivity.kt
@@ -5,7 +5,7 @@ import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
-import com.codewithkael.webrtcdatachannelty.R
+//import com.codewithkael.webrtcdatachannelty.R
 import com.codewithkael.webrtcdatachannelty.databinding.ActivitySplashBinding
 
 class SplashActivity : AppCompatActivity() {
